760e54994bf6f6dc5685283e0c87625a68238c15
Calebe Cavalcante <calebesantos.cavalcante@gmail.com>
2017-11-23 13:38:50 -0200

    Create fn_is_number.sql

---
+++ fn_is_number.sql
@@ -0,0 +1,11 @@
+create or replace FUNCTION IS_NUMBER (p_string IN VARCHAR2)
+   RETURN INT
+IS
+   v_new_num NUMBER;
+BEGIN
+   v_new_num := TO_NUMBER(p_string);
+   RETURN 1;
+EXCEPTION
+WHEN VALUE_ERROR THEN
+   RETURN 0;
+END IS_NUMBER;
